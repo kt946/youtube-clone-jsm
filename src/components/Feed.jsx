71eb0804feab3ce4ed3a02a7fb9e9f46cd66bb6a
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-01-09 16:44:47 -0500

    updated VideoDetail component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -7,9 +7,11 @@ import { Sidebar, Videos } from './';
 const Feed = () => {
 
   const [selectedCategory, setSelectedCategory] = useState('New');
-  const [videos, setVideos] = useState([]);
+  const [videos, setVideos] = useState(null);
 
   useEffect(() => {
+    setVideos(null);
+
     fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
       .then((data) => {setVideos(data.items)});
   }, [selectedCategory]);
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -5,12 +5,16 @@ import { CheckCircle } from '@mui/icons-material';
 import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from '../utils/constants';
 
 const VideoCard = ({ video: { id: { videoId }, snippet} }) => (
-  <Card sx={{ width: { md: '320px', xs: '100% '}, boxShadow: 'none', borderRadius: 0 }}>
+  <Card sx={{ width: { xs: '100%', sm: '358px', md: '320px' }, boxShadow: 'none', borderRadius: 0 }}>
     <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
       <CardMedia 
         image={snippet?.thumbnails?.high?.url || demoThumbnailUrl} 
         alt={snippet?.title}
-        sx={{ width: 358, height:180 }}
+        sx={{ width: {
+          xs: '100%',
+          sm: '358px',
+          md: '320px'
+        }, height:180 }}
       />
     </Link>
     <CardContent 
