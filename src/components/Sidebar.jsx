788de6fb6cee466c87f45a22479aa3535fad870e
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-01-09 00:38:29 -0500

    updated Feed and Sidebar components with data from API

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,11 @@
 import { Stack } from '@mui/material';
 import { categories } from '../utils/constants';
 
-const selectedCategory = 'New';
-
-const Sidebar = () => (
+const Sidebar = ({ selectedCategory, setSelectedCategory }) => (
   <Stack
     direction="row"
     sx={{ 
+      overflowY: "auto",
       height: { sx: 'auto', md: '95%' },
       flexDirection: { md: 'column'},
     }}
@@ -14,6 +13,7 @@ const Sidebar = () => (
     {categories.map((category) => (
       <button
         className="category-btn"
+        onClick={() => setSelectedCategory(category.name)}
         style={{
           background: category.name === selectedCategory && '#FC1503',
           color: 'white'
